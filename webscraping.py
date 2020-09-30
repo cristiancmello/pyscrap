4ce4d039669b45cc150f0d2d09f6b4cf5e4873f7
Cristian Mello <cristianc.mello@gmail.com>
2020-09-30 20:49:27 -0300

    Add script to get 10 top scores from NBA website

--- webscraping.py
+++ webscraping.py
@@ -16,5 +16,24 @@ binary = FirefoxBinary('/usr/bin/firefox')
 driver = webdriver.Firefox()
 
 driver.get(url)
+# time.sleep(5)
+
+driver.find_element_by_xpath("//div//div[@class='banner-actions-container']//button").click()
+time.sleep(1)
+
+# driver.find_element_by_xpath("//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
+
+element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
+html_content = element.get_attribute('outerHTML')
+
+soup = BeautifulSoup(html_content, 'html.parser')
+table = soup.find(name='table')
+
+df_full = pd.read_html(str(table))[0].head(10)
+df = df_full[['Unnamed: 0', 'PLAYER', 'TEAM', 'PTS']]
+df.columns = ['pos', 'player', 'team', 'total']
+
+print(df)
+
 
 driver.quit()
