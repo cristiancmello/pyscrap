4b83e06c9660a5eaf1592fb16c3a553422f34dc0
Cristian Mello <cristianc.mello@gmail.com>
2020-09-30 21:34:39 -0300

    Refactoring webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -8,7 +8,35 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 import json
 
-url = 'https://stats.nba.com/players/traditional/?sort=PTS&dir=-1'
+url = 'https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1'
+top10ranking = {}
+
+rankings = {
+  '3points': {'field': 'FG3M', 'label': '3PM'},
+  'points': {'field': 'PTS', 'label': 'PTS'},
+  'assistants': {'field': 'AST', 'label': 'AST'},
+  'rebounds': {'field': 'REB', 'label': 'REB'},
+  'steals': {'field': 'STL', 'label': 'STL'},
+  'blocks': {'field': 'BLK', 'label': 'BLK'},
+}
+
+def buildrank(type):
+  field = rankings[type]['field']
+  label = rankings[type]['label']
+
+  driver.find_element_by_xpath(f"//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='{field}']").click()
+
+  element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
+  html_content = element.get_attribute('outerHTML')
+
+  soup = BeautifulSoup(html_content, 'html.parser')
+  table = soup.find(name='table')
+
+  df_full = pd.read_html(str(table))[0].head(10)
+  df = df_full[['Unnamed: 0', 'PLAYER', 'TEAM', label]]
+  df.columns = ['pos', 'player', 'team', 'total']
+
+  return df.to_dict('records')
 
 option = Options()
 option.headless = True
@@ -16,24 +44,19 @@ binary = FirefoxBinary('/usr/bin/firefox')
 driver = webdriver.Firefox()
 
 driver.get(url)
-# time.sleep(5)
+driver.implicitly_wait(10)  # in seconds
 
 driver.find_element_by_xpath("//div//div[@class='banner-actions-container']//button").click()
 time.sleep(1)
 
-# driver.find_element_by_xpath("//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']").click()
+top10ranking = {}
+top10ranking['points'] = buildrank('points')
 
-element = driver.find_element_by_xpath("//div[@class='nba-stat-table']//table")
-html_content = element.get_attribute('outerHTML')
-
-soup = BeautifulSoup(html_content, 'html.parser')
-table = soup.find(name='table')
-
-df_full = pd.read_html(str(table))[0].head(10)
-df = df_full[['Unnamed: 0', 'PLAYER', 'TEAM', 'PTS']]
-df.columns = ['pos', 'player', 'team', 'total']
-
-print(df)
-
+for k in rankings:
+  top10ranking[k] = buildrank(k)
 
 driver.quit()
+
+with open('ranking.json', 'w', encoding='utf-8') as jp:
+  js = json.dumps(top10ranking, indent=4)
+  jp.write(js)
